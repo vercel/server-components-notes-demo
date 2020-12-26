dc6daba1081225505ad0614ca01bffb5e7b745bf
Shu Ding <g@shud.in>
2020-12-27 06:45:31 +0800

    add more metrics

--- api/react.server.js
+++ api/react.server.js
@@ -1,5 +1,7 @@
 const sendRes = require('../libs/send-res.server')
 
 export default async (req, res) => {
+  console.time('react.server.js')
   await sendRes(req, res, null)
+  console.timeEnd('react.server.js')
 }
