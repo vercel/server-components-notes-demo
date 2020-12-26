67a91f9beb8a9f163588370a86e93931a531bba8
Shu Ding <g@shud.in>
2020-12-27 06:57:13 +0800

    stream to res

--- api/react.server.js
+++ api/react.server.js
@@ -1,7 +1,5 @@
 const sendRes = require('../libs/send-res.server')
 
 export default async (req, res) => {
-  console.time('react.server.js')
   await sendRes(req, res, null)
-  console.timeEnd('react.server.js')
 }
--- libs/send-res.src.js
+++ libs/send-res.src.js
@@ -1,28 +1,6 @@
 const {pipeToNodeWritable} = require('react-server-dom-webpack/writer.node.server')
-const stream = require('stream')
-
-// https://webpack.js.org/api/module-variables/#__non_webpack_require__-webpack-specific
-// const nodeRequire = __non_webpack_require__
-
-// const babelRegister = require('@babel/register')
-
-// require('@babel/plugin-transform-modules-commonjs')
-// require('@babel/preset-react')
-
-// babelRegister({
-//   presets: ['@babel/preset-react'],
-//   plugins: ['@babel/transform-modules-commonjs'],
-// })
-
-// lambda deps
-// require('react-fetch')
-// require('date-fns')
-// require('excerpts')
-// require('marked')
-// require('sanitize-html')
 
 const React = require('react')
-// require('react-server-dom-webpack/node-register')()
 const ReactApp = require('../components/App.server').default
 
 const endpoint = process.env.ENDPOINT
@@ -30,41 +8,25 @@ const fetch = require('node-fetch').default
 
 let moduleMap
 
-async function generate(arg) {
-  async function renderReactTree(props, cb) {
-    if (!moduleMap) {
-      const response = await fetch(endpoint + '/react-client-manifest.json')
-      const originalManifest = await response.json()
-      const manifest = {}
+async function renderReactTree(props, res) {
+  if (!moduleMap) {
+    const response = await fetch(endpoint + '/react-client-manifest.json')
+    const originalManifest = await response.json()
+    const manifest = {}
 
-      // hack: we need to modify the filepath in the manifest
-      // and proxy it to unify the map
-      for (let key in originalManifest) {
-        manifest[key.split('/').pop()] = originalManifest[key]
-      }
-      moduleMap = new Proxy(manifest, {
-        get: function(target, prop) {
-          return target[prop.split('/').pop()]
-        }
-      })
+    // hack: we need to modify the filepath in the manifest
+    // and proxy it to unify the map
+    for (let key in originalManifest) {
+      manifest[key.split('/').pop()] = originalManifest[key]
     }
-    
-    let data = ''
-    const dest = new stream.Writable({
-      write: (chunk, _, callback) => {
-        data += chunk
-        callback()
-      },
-      final: () => {
-        cb(data)
+    moduleMap = new Proxy(manifest, {
+      get: function(target, prop) {
+        return target[prop.split('/').pop()]
       }
     })
-
-    pipeToNodeWritable(React.createElement(ReactApp, props), dest, moduleMap)
   }
 
-  // await renderReactTree(JSON.parse(process.argv[2]))
-  return await new Promise(res => renderReactTree(arg, res))
+  pipeToNodeWritable(React.createElement(ReactApp, props), res, moduleMap)
 }
 
 module.exports = async (req, res, redirectToId) => {
@@ -74,13 +36,9 @@ module.exports = async (req, res, redirectToId) => {
   }
   res.setHeader('X-Location', JSON.stringify(location))
 
-  console.time('react render')
-  const output = await generate({
+  renderReactTree({
     selectedId: location.selectedId,
     isEditing: location.isEditing,
     searchText: location.searchText,
-  })
-  console.timeEnd('react render')
-
-  res.end(output)
+  }, res)
 }
