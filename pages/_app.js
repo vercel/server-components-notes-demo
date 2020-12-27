cffb197bd5fcd411b69f65736d1d6fefdfc170b9
Shu Ding <g@shud.in>
2020-12-28 00:17:53 +0800

    add preload header

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,11 @@ export default function MyApp({ Component, pageProps }) {
       <meta name="twitter:url" content="https://next-server-components.vercel.app/og.png"/>
       <meta name="og:title" content="React Server Components (Experimental Demo)"/>
       <meta name="og:image" content="https://next-server-components.vercel.app/og.png"/>
+      <link
+        rel="preload"
+        as="fetch"
+        crossOrigin="anonymous"
+        href={'https://next-server-components.vercel.app/api?location=%7B%22selectedId%22%3Anull%2C%22isEditing%22%3Afalse%2C%22searchText%22%3A%22%22%7D'} />
     </Head>
     <Component {...pageProps} />
   </>
